fe7f3b86077a871bc70d405eafd7d1a27f261482
stefan <sandersson@fastmail.fm>
2019-07-15 10:07:31 +0200

    some select fixes and tweaks

--- static_files/js/match.js
+++ static_files/js/match.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+    $("form").submit(function() {
+        $("#id_player1").prop("disabled", false);
+        $("#id_player2").prop("disabled", false);
+    });
+
     if(window.location.href.indexOf("/change/") > -1) {
         // only filter on the change page
         var player_1_id = $('#id_player1').val();
@@ -11,5 +16,11 @@ $(document).ready(function() {
                 }
             }
         });
+
+        console.log($("#id_player1").select2('destroy'));
+        console.log($("#id_player2").select2('destroy'));
+        $("#id_player1").prop('disabled', 'disabled');
+        $("#id_player2").prop('disabled', 'disabled');
+
     }
 });
